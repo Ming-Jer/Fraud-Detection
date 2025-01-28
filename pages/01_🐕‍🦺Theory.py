887b4951d20506ea1210cedda1e1fe690057ee41
TGI-ed <ed@gideons.tw>
2025-01-28 12:20:39 +0800

    Add fradu detection

--- pages/01_🐕‍🦺Theory.py
+++ pages/01_🐕‍🦺Theory.py
@@ -17,14 +17,25 @@ st.sidebar.info(markdown)
 logo = "./images/MIT-Fraud-Detection-PRESS.jpg"
 st.sidebar.image(logo)
 
-st.header("詐欺偵測簡介 (Introduction to Fraud Detection)")
+st.subheader("詐欺偵測簡介 (Introduction to Fraud Detection)")
 
-tab_forward, tab_fds, tab_ml, tab_ref, tab_ad = st.tabs(["前言", "詐欺偵測", "機器學習","參考文獻", "異常偵測範例"])
+tab_forward, tab_cfs, tab_fds, tab_ml, tab_ref, tab_ad = st.tabs(["前言", "詐欺偵測場景", "詐欺偵測", "機器學習","參考文獻", "異常偵測範例"])
 with tab_forward:
     st.image("./images/MIT-Fraud-Detection-PRESS.jpg", caption="Fraud Detection")
     # reading markdown file
     cwd = os.getcwd()
     intro_markdown = read_markdown_file(cwd+'/docs/Forward.md')
+    st.markdown(intro_markdown, unsafe_allow_html=True)
+
+with tab_cfs:
+    st.image("./images/SEPA_FraudVolumePerType.png", caption="Fraud Volume")
+    st.write("使用在 SEPA 內發行的卡進行的信用卡欺詐總價值的演變。無卡欺詐(圖中黃色部分)佔報告的欺詐的大部分。")
+    st.image("./images/SEPA_FraudType_CardPresent.png", caption="Fraud Type")
+    st.write("SEPA 內按類別劃分的持卡欺詐價值的演變和細分。")
+
+    # reading markdown file
+    cwd = os.getcwd()
+    intro_markdown = read_markdown_file(cwd+'/docs/FraudDetection.md')
     st.markdown(intro_markdown, unsafe_allow_html=True)
 
 with tab_fds:
@@ -34,9 +45,21 @@ with tab_fds:
     intro_markdown = read_markdown_file(cwd+'/docs/FDS.md')
     st.markdown(intro_markdown, unsafe_allow_html=True)
 
+with tab_ml:
+    # reading markdown file
+    cwd = os.getcwd()
+    intro_markdown = read_markdown_file(cwd+'/docs/Lecture_ML.md')
+    st.markdown(intro_markdown, unsafe_allow_html=True)
+
+with tab_ref:
+    # reading markdown file
+    cwd = os.getcwd()
+    intro_markdown = read_markdown_file(cwd+'/docs/References.md')
+    st.markdown(intro_markdown, unsafe_allow_html=True)
+
 with tab_ad:
     markdown="""
-    #### 線性異常偵測範例 Linear Anomaly Detection Example
+    #### 單維度異常偵測範例 Simple Anomaly Detection Example
     - 假設單一陣列/列表中包含某一群體的智商分數分布 Assuming a single array/list of data contains a distribution of IQ scores within a given population
     """
     st.markdown(markdown)
@@ -62,20 +85,8 @@ with tab_ad:
     st.pyplot(fig)
 
     markdown="""
-    - 雖然這些技術適用於一維資料（即單一欄位），但在高維度的情況下，這些方法就變得過於複雜。While these techniques work for 1d data i.e. single columns, the higher-dimensional case is too complicated for these approaches.
-    - 因此需要採用非線性方法，因為我們必須確定特徵空間中不同欄位的資料如何相互作用，從而增強方法的預測能力。A non-linear approach is therefore required, as we need to determine how the data from different columns in the feature space interacts with each other, adding to the predictive ability of the approach.
+    - 雖然這些技術適用於一維資料（即單一欄位），但高維度資料對這些方法而言，會變得太過複雜。While these techniques work for 1d data i.e. single columns, the higher-dimensional case is too complicated for these approaches.
+    - 因此，我們需要採用非線性方法，以便了解特徵空間中不同欄位的資料如何相互影響，從而提升預測能力。A non-linear approach is therefore required, as we need to determine how the data from different columns in the feature space interacts with each other, adding to the predictive ability of the approach.
 
     """
     st.markdown(markdown)
-
-with tab_ml:
-    # reading markdown file
-    cwd = os.getcwd()
-    intro_markdown = read_markdown_file(cwd+'/docs/Lecture_ML.md')
-    st.markdown(intro_markdown, unsafe_allow_html=True)
-
-with tab_ref:
-    # reading markdown file
-    cwd = os.getcwd()
-    intro_markdown = read_markdown_file(cwd+'/docs/References.md')
-    st.markdown(intro_markdown, unsafe_allow_html=True)
