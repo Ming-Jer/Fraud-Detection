de2f37b06b09976126d2415ce0be16152e9fb082
TGI-ed <ed@gideons.tw>
2025-01-29 19:42:37 +0800

    update transcation

---
+++ docs/Simulator_transactions.md
@@ -0,0 +1,12 @@
+## 2.4. 交易資料生成
+
+客戶檔案現在已包含我們生成交易所需的所有資訊。交易生成將透過`generate_transactions_table`函式來執行，該函式接受客戶檔案、起始日期以及要生成交易的天數作為輸入參數。它將回傳一個交易表格，格式如上所述（尚未包含交易標記，這部分將在[詐欺情境生成](https://fraud-detection-handbook.github.io/fraud-detection-handbook/Chapter_3_GettingStarted/SimulatedDataset.html#fraud-scenarios-generation)階段加入）。
+
+讓我們以第一位客戶為例，從2018-04-01開始生成五天的交易：
+
+我們可以快速檢查生成的交易是否符合客戶檔案的屬性：
+
+- 終端機ID確實來自可用終端機清單（0、1、2和3）
+- 交易金額似乎符合客戶的金額參數（`mean_amount`=62.26和`std_amount`=31.13）
+- 每日交易次數根據客戶的交易頻率參數（`mean_nb_tx_per_day`=2.18）而變化
+
--- pages/02_🎰Simulator.py
+++ pages/02_🎰Simulator.py
@@ -15,6 +15,7 @@ from fds_subs import read_markdown_file
 from fds_subs import generate_customer_profiles_table
 from fds_subs import generate_terminal_profiles_table
 from fds_subs import get_list_terminals_within_radius
+from fds_subs import generate_transactions_table
 
 
 # Customize the sidebar
@@ -24,7 +25,8 @@ fds_sidebar()
 st.markdown("<div id='linkto_top'></div>", unsafe_allow_html=True) 
 st.subheader("交易資料模擬器 (Transaction data simulator)")
 
-tab_intro, tab_customer, tab_terminal, tab_terminal_list = st.tabs(["模擬器簡介","客戶資料", "終端機配置", "客戶與終端機關聯"])
+# Tabs Menu
+tab_intro, tab_customer, tab_terminal, tab_terminal_list, tab_transactions = st.tabs(["模擬器簡介","客戶資料", "終端機配置", "客戶與終端機關聯", "交易生成"])
 
 with tab_intro:
     # reading markdown file
@@ -40,12 +42,13 @@ with tab_customer:
     st.markdown(intro_markdown, unsafe_allow_html=True)
 
     st.write("產生的客戶資料")
-    with st.expander("顯示原始碼See Source Code"):
+    with st.expander("顯示原始碼 See Source Code"):
         with st.echo():
             n_customers = 5
             customer_profiles_table = generate_customer_profiles_table(n_customers, random_state = 0)
+            st.dataframe(customer_profiles_table, hide_index = True)
             
-    st.dataframe(customer_profiles_table)
+    st.dataframe(customer_profiles_table, hide_index = True)
     # add the link at the bottom of each page
     st.markdown("<a href='#linkto_top'>返回頁首(Top)</a>", unsafe_allow_html=True)
 
@@ -54,12 +57,13 @@ with tab_terminal:
     intro_markdown = read_markdown_file(os.getcwd()+'/docs/Simulator_terminal.md')
     st.markdown(intro_markdown, unsafe_allow_html=True)
 
-    with st.expander("顯示原始碼See Source Code"):
+    with st.expander("顯示原始碼 See Source Code"):
         with st.echo():
             n_terminals = 5
             terminal_profiles_table = generate_terminal_profiles_table(n_terminals, random_state = 0)
+            st.dataframe(terminal_profiles_table, hide_index = True)
 
-    st.dataframe(terminal_profiles_table)
+    st.dataframe(terminal_profiles_table, hide_index = True)
 
     # add the link at the bottom of each page
     st.markdown("<a href='#linkto_top'>返回頁首(Top)</a>", unsafe_allow_html=True)
@@ -69,12 +73,14 @@ with tab_terminal_list:
     intro_markdown = read_markdown_file(os.getcwd()+'/docs/Simulator_list_terminal.md')
     st.markdown(intro_markdown, unsafe_allow_html=True)
 
-    with st.expander("顯示原始碼"):
+    with st.expander("顯示原始碼 See Source Code"):
         with st.echo():
             # We first get the geographical locations of all terminals as a numpy array
             x_y_terminals = terminal_profiles_table[['x_terminal_id','y_terminal_id']].values.astype(float)
             # And get the list of terminals within radius of $50$ for the last customer
-            get_list_terminals_within_radius(customer_profiles_table.iloc[4], x_y_terminals=x_y_terminals, r=50)
+            avaiale_terminials=get_list_terminals_within_radius(customer_profiles_table.iloc[4], x_y_terminals=x_y_terminals, r=50)
+            st.write(avaiale_terminials)
+    st.write(avaiale_terminials)
     
     intro_markdown="""
     為了更好的視覺化，讓我們繪製
@@ -115,16 +121,49 @@ with tab_terminal_list:
             ax.set_xlabel('x_terminal_id', fontsize=fontsize)
             ax.set_ylabel('y_terminal_id', fontsize=fontsize)
             st.pyplot(terminals_available_to_customer_fig)
+    
+    st.pyplot(terminals_available_to_customer_fig)
 
     intro_markdown="""
-    使用 panda 的 apply 函數來計算每個客戶可用的終端機清單相當簡單直接。我們將結果儲存為客戶資料表中的新欄位 available_terminals。
+    使用 pandas 的 `apply` 函式來計算每位客戶可用的終端機清單相當簡單直觀。我們將結果儲存為客戶檔案表中的新欄位 `available_terminals`。
+    值得注意的是，半徑 $r$ 控制了每位客戶平均可用的終端機數量。隨著終端機數量的增加，應該調整此半徑以符合模擬中期望的每位客戶平均可用終端機數量。
     """
     st.markdown(intro_markdown, unsafe_allow_html=True)
     with st.expander("顯示原始碼"):
         with st.echo():
             customer_profiles_table['available_terminals']=customer_profiles_table.apply(lambda x : get_list_terminals_within_radius(x, x_y_terminals=x_y_terminals, r=50), axis=1)
+            st.dataframe(customer_profiles_table, hide_index = True)
     
     st.dataframe(customer_profiles_table)
+
+    # add the link at the bottom of each page
+    st.markdown("<a href='#linkto_top'>返回頁首(Top)</a>", unsafe_allow_html=True)
+
+with tab_transactions:
+    # reading markdown file
+    intro_markdown = read_markdown_file(os.getcwd()+'/docs/Simulator_transactions.md')
+    st.markdown(intro_markdown, unsafe_allow_html=True)
+
+    with st.expander("顯示原始碼 See Source Code"):
+        with st.echo():
+            transaction_table_customer_0=generate_transactions_table(customer_profiles_table.iloc[0], 
+                                                                start_date = "2018-04-01", 
+                                                                nb_days = 5)
+            st.dataframe(transaction_table_customer_0, hide_index = True)
+
+    st.dataframe(transaction_table_customer_0, hide_index = True)
+
+    intro_markdown="""
+    現在讓我們為所有客戶生成交易。這可以使用pandas的`groupby`和`apply`方法輕鬆完成：
+    這樣我們就得到了一組包含5位客戶、5個終端機、5天期間內共65筆交易的資料集。
+    """
+    st.markdown(intro_markdown, unsafe_allow_html=True)
+    with st.expander("顯示原始碼 See Source Code"):
+        with st.echo():
+            transactions_df=customer_profiles_table.groupby('CUSTOMER_ID').apply(lambda x : generate_transactions_table(x.iloc[0], nb_days=5)).reset_index(drop=True)
+            st.dataframe(transactions_df, hide_index = True)
+            
+    st.dataframe(transactions_df, hide_index = True)
 
     # add the link at the bottom of each page
     st.markdown("<a href='#linkto_top'>返回頁首(Top)</a>", unsafe_allow_html=True)
