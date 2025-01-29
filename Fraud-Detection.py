76237e2423ea08544d529b8758de0b52e575f6f9
TGI-ed <ed@gideons.tw>
2025-01-29 16:50:41 +0800

    update image from github

--- Fraud-Detection.py
+++ Fraud-Detection.py
@@ -16,14 +16,14 @@ st.markdown(
 st.subheader("請選擇左列功能列功能")
 
 markdown = """
-1. Theory 背景知識、理論與發展現況
-2. 交易資料模擬器
+1. Theory-背景知識、理論與發展現況
+2. Simulator-交易資料模擬器
 3. 機器學習方法
 4. 機器學習效能評估
 5. 機器學習模型驗證與模型選擇
 6. 非平衡資料演算法
 7. 深度學習
-8. Fraud Detection dashboard 詐欺偵測儀表板
+8. Fraud Detection dashboard-詐欺偵測儀表板
 
 """
 
