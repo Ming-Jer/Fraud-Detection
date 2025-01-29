a8259c38b9e3a400b1761cb28b9ed5ae196fae15
TGI-ed <ed@gideons.tw>
2025-01-29 08:53:21 +0800

    update sim-2

--- pages/02_Simulator.py
+++ pages/02_Simulator.py
@@ -1,6 +1,48 @@
 import streamlit as st
 import pandas as pd
-from shared_functions import generate_customer_profiles_table
+import os
+
+import numpy as np
+import pandas as pd
+
+import datetime
+import time
+
+import random
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+sns.set_style('darkgrid', {'axes.facecolor': '0.9'})
+
+def generate_customer_profiles_table(n_customers, random_state=0):
+    
+    np.random.seed(random_state)
+        
+    customer_id_properties=[]
+    
+    # Generate customer properties from random distributions 
+    for customer_id in range(n_customers):
+        
+        x_customer_id = np.random.uniform(0,100)
+        y_customer_id = np.random.uniform(0,100)
+        
+        mean_amount = np.random.uniform(5,100) # Arbitrary (but sensible) value 
+        std_amount = mean_amount/2 # Arbitrary (but sensible) value
+        
+        mean_nb_tx_per_day = np.random.uniform(0,4) # Arbitrary (but sensible) value 
+        
+        customer_id_properties.append([customer_id,
+                                      x_customer_id, y_customer_id,
+                                      mean_amount, std_amount,
+                                      mean_nb_tx_per_day])
+        
+    customer_profiles_table = pd.DataFrame(customer_id_properties, columns=['CUSTOMER_ID',
+                                                                      'x_customer_id', 'y_customer_id',
+                                                                      'mean_amount', 'std_amount',
+                                                                      'mean_nb_tx_per_day'])
+    
+    return customer_profiles_table
 
 n_customers = 5
 customer_profiles_table = generate_customer_profiles_table(n_customers, random_state = 0)
