72fe81a56e37d844db7aa2b8a61b3803c5e4ef20
TGI-ed <ed@gideons.tw>
2025-01-31 22:43:34 +0800

    try to resolve packages issues

--- pyFraudDetection/simulator/__init__.py
+++ pyFraudDetection/simulator/__init__.py
@@ -1,11 +1,11 @@
 # Export functions from the sim_modules sub-package
-from .sim_modules import generate_customer_profiles_table
-from .sim_modules import generate_terminal_profiles_table
-from .sim_modules import get_list_terminals_within_radius
-from .sim_modules import generate_transactions_table
-from .sim_modules import generate_dataset
-from .sim_modules import add_frauds
-from .sim_modules import get_stats
+from .sim_cores import generate_customer_profiles_table
+from .sim_cores import generate_terminal_profiles_table
+from .sim_cores import get_list_terminals_within_radius
+from .sim_cores import generate_transactions_table
+from .sim_cores import generate_dataset
+from .sim_cores import add_frauds
+from .sim_cores import get_stats
 
 from .sim_UI import sim_tab_intro
 from .sim_UI import sim_tab_customer
---
+++ pyFraudDetection/simulator/sim_cores/__init__.py
@@ -0,0 +1,8 @@
+# Export functions from the sim_modules sub-package
+from .sim_modules import generate_customer_profiles_table
+from .sim_modules import generate_terminal_profiles_table
+from .sim_modules import get_list_terminals_within_radius
+from .sim_modules import generate_transactions_table
+from .sim_modules import generate_dataset
+from .sim_modules import add_frauds
+from .sim_modules import get_stats
