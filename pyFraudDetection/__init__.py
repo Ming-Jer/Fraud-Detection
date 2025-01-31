4cfaf391a8a519ea4a097be0a50c5b419904dfda
TGI-ed <ed@gideons.tw>
2025-01-31 22:50:43 +0800

    remove sim_UI.py

--- pyFraudDetection/__init__.py
+++ pyFraudDetection/__init__.py
@@ -12,7 +12,3 @@ from .simulator import generate_transactions_table
 from .simulator import generate_dataset
 from .simulator import add_frauds
 from .simulator import get_stats
-
-from .simulator import sim_tab_customer
-from .simulator import sim_tab_terminal
-from .simulator import sim_tab_list
